bf469ec4d517f301288f16a9de9bdf53dc0451cb
Arthur <nazera55tigr66t28@gmail.com>
2024-11-12 13:23:17 +0200

    fix utf-8 and snake type

--- СS_LRU_WITH_MRU/MRU_Cache.cpp
+++ СS_LRU_WITH_MRU/MRU_Cache.cpp
@@ -3,37 +3,37 @@
 namespace CacheLibrary {
 
     void MRU_Cache::insert(const int key, int value) {
-        if (!keyMap.contains(key)) {
-            if (cacheKeys.size() >= capacity) {
-	            const int first_key = cacheKeys.front();
-                cacheKeys.pop_front();
-                keyMap.erase(first_key);
+        if (!key_map_.contains(key)) {
+            if (cache_keys_.size() >= capacity_) {
+	            const int first_key = cache_keys_.front();
+                cache_keys_.pop_front();
+                key_map_.erase(first_key);
                 std::cout << "MRU: Delete the most recent key: " << first_key << '\n';
             }
         }
         else {
-            cacheKeys.erase(keyMap[key]);
+            cache_keys_.erase(key_map_[key]);
         }
-        cacheKeys.push_back(key);
-        keyMap[key] = --cacheKeys.end();
+        cache_keys_.push_back(key);
+        key_map_[key] = --cache_keys_.end();
         std::cout << "MRU: Adding a key: " << key << " to the MRU cache." << '\n';
     }
 
     int MRU_Cache::get(const int key) {
-        if (keyMap.contains(key)) {
-            cacheKeys.splice(cacheKeys.end(), cacheKeys, keyMap[key]);
-            return *keyMap[key];
+        if (key_map_.contains(key)) {
+            cache_keys_.splice(cache_keys_.end(), cache_keys_, key_map_[key]);
+            return *key_map_[key];
         }
         return -1;
     }
 
     bool MRU_Cache::contains(const int key) const {
-        return keyMap.contains(key);
+        return key_map_.contains(key);
     }
 
     void MRU_Cache::display_status() const {
         std::cout << "MRU Cache Status:\n";
-        for (const int key : cacheKeys) {
+        for (const int key : cache_keys_) {
             std::cout << "Key: " << key << " ";
         }
         std::cout << '\n';
